c1135ca86281f169f6139f5145fadfe780ea199f
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-09 02:47:44 +0100

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -1,1 +1,29 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+const fs = require("fs");
 
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  
+  bot.user.setActivity("Všechny prémium! [P>]", {type: "WATCHING"});
+});
+
+bot.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+  let prefix = "P>"
+  
+  if(cmd === `${prefix}help`){
+    var embed = new Discord.RichEmbed
+    .setTitle("Pomoc!")
+    .setColor("GREEN")
+    .addField("P>say", "Bot řekne něco")
+    .setTimestamp()
+    .setFooter("Bot vytvořen JustNela#6666");
+    message.channel.sendEmbed(embed)
+    return;
+  }
+});
+
+bot.login(process.env.token);
