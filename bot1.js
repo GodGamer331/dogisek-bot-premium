261d593aef105aa40a45ee8a16753376628d4efd
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-09 04:09:26 +0100

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -17,12 +17,17 @@ bot.on("message", async message => {
   let messageArray = message.content.split(" ")
   let cmd = messageArray[0]
   let args = messageArray.slice(1)
-  //ping command
+  let mods = message.guild.roles.find("name", "Moderator");
+  let premium = message.guild.roles.find("name", "★†Premium†★");
+  
+  //ping command ↓
+  
+  if(!premium) return message.channel.send("Nejsi prémium!")
   if(cmd === `${prefix}ping`){
     var embed = new Discord.RichEmbed()
     .setTitle("Ping")
     .addField(":ping_pong:", "Pong!");
-    message.content.sendMessage(embed)
+    message.content.send(embed);
     return;
   }
 });
