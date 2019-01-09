922d123ad2dad974d2b40edc7c91263dac0c59b3
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-09 03:39:16 +0100

    Update bot1.js

--- bot1.js
+++ bot1.js
@@ -1,19 +1,27 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 const fs = require("fs");
+const prefix = "P>";
 
 bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
+  console.log(`${bot.user.username} is online with prefix P>!`);
   
   bot.user.setActivity("Všechny prémium! [P>]", {type: "WATCHING"});
 });
 
 bot.on("message", function(message){
   if(message.author.equals(bot.user)) return;
+  if(!message.content.startsWith(prefix)) return;
   
-  if(message.content == "Dogisrk Bot Premium"){
-    message.channel.sendMessage("Muj prefix je P>");
+  var args = message.content.substring(prefix.lenght).split(" ");
+  
+  switch (args[0]){
+      case "ping":
+      message.content.sendMessage(":ping_pong: Pong!");
+      break;
+      
   }
+    
 });
 
 
